5cb0d1af7964b1858e54090a28059f4203eb3f74
Ailin Li <ailin.li@wealthsimple.com>
2022-05-16 17:45:25 -0400

    Add routes for new, create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
