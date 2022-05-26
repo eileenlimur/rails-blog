6f7fc8719ab012aad094e79991bb5fb8c507e892
Ailin Li <ailin.li@wealthsimple.com>
2022-05-25 23:46:52 -0400

    Create public_count class method and use in view

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -7,6 +7,11 @@ module Visible #this method applies to individual instance
         validates :status, inclusion: { in: VALID_STATUSES }
     end
 
+    class_methods do
+        def public_count
+            where(status: 'public').count
+        end
+    end
 
     def archived?
         status == 'archived'
